550999cdad319d345d8a9adc6d3ad08889b69479
StanislavHorb <s.a.gorb@student.khai.edu>
2021-10-06 17:15:09 +0300

    2-nd

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,2 +1,12 @@
 print('HANGMAN')
 print('The game will be available soon')
+import random
+my_list = ['Python', 'java', 'javascript']
+
+a = 'python'
+print ('Введите слово!')
+b = str(input())
+if b == a:
+    print('Вы выжили!')
+else:
+    print('Вы погибли!')
