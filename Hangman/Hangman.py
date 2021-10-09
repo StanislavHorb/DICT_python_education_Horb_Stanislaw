77311b192b5d7da8e4398ea534d4c06d2c0c57b5
StanislavHorb <s.a.gorb@student.khai.edu>
2021-10-09 18:57:27 +0300

    1-st

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,6 +1,14 @@
+import random
+from my_list import my_list
+
+def get_valid_word(my_list):
+    word = random.choice(my_list)
+    while '-' in word or ' ' in word:
+        word = random.choice(my_list)
+
+        return word.upper()
 print('HANGMAN')
 print('The game will be available soon')
-import random
 my_list = ['Python', 'java', 'javascript']
 
 a = 'python'
