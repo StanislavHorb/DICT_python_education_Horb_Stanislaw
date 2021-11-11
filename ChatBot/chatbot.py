75e753c61b724686c612d3243759c539b1cffa07
StanislavHorb <s.a.gorb@student.khai.edu>
2021-11-11 11:58:21 +0200

    1-st stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -8,7 +8,7 @@ print("Enter remainders of divining your age by 3, 5 and 7")
 a = int(input())
 b = int(input())
 c = int(input())
-age = (a*70+b*21+c*15)%105
+age = (a * 70 + b * 21 + c * 15) % 105
 print("Your age is " + str(age) + " that's a good time to start programming!")
 print("Now I will prove to you that I can count to any number you want.")
 num = int(input())
@@ -23,11 +23,15 @@ Why do we use methods?
 3. To determine the execution time of a program.
 4. To interrupt the execution of a program.
 """)
-def answers(myanswer):
+
+
+def answers():
     answer = int(input())
     if answer == 2:
         print("Congratulations, have a nice day!")
     else:
         print("Please, try again.")
-        answers("1")
-answers("2")
+        answers()
+
+
+answers()
--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -1,7 +1,29 @@
-print ("""Starting to make a coffee
+print("""Starting to make a coffee
 Grinding coffee beans
 Boiling water
 Mixing boiled water with crushed coffee beans
 Pouring coffee into the cup
 Pouring some milk into the cup
 Coffee is ready!""")
+water = 200
+milk = 50
+coffee = 15
+
+a = int(input("Write how many cups of coffee you will need:"))
+b = str(input("Write your favorite coffee:"))
+
+if a == 1:
+    print("For " + str(a) + " cup of coffee you will need:")
+    print('water ' + str(water) + ' ml')
+    print('milk ' + str(milk) + ' ml')
+    print('coffee ' + str(coffee) + ' g')
+elif a > 1:
+    print("For " + str(a) + " cups of coffee you will need:")
+    print('water ' + str(a * water) + ' ml')
+    print('milk ' + str(a * milk) + ' ml')
+    print('coffee ' + str(a * coffee) + ' g')
+
+print("What is your favorite coffee?")
+my_list = ['americano', 'cappuccino', 'espresso', 'latte']
+if b == 'americano':
+    else
